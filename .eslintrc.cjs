fd3cad612e10f90acb4747aa47fd11bfde58032a
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-06-02 00:06:37 -0300

    Delete unnecessary files, clean App component, add esling to plugins in vite.config.js, and add two new rules in eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,5 +13,7 @@ module.exports = {
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "no-unused-vars": "warn",
+    "react/prop-types": "off",
   },
 };

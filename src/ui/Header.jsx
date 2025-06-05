16d710667b969c6afd73fb37edae283682677a4c
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-06-04 21:54:18 -0300

    Add the layout route of the entire application which uses the new AppLayout component and it is the parent route of all the other ones, and this new AppLayout uses the new Header component, the Outlet component to render the children routes, and the CartOverview component for now

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Matías</p>
+    </header>
+  );
+}
+
+export default Header;

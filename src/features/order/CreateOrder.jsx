0ea45e16f26f96c3dd43ac4ebaf9a0ad8dfbb91c
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-06-11 00:06:44 -0300

    Handle errors in Form actions: Add a new errors object in the action function, check if a valid phone number has been entered and if not, then add the error and return the errors object before creating the new order. Afterwards, with useNavigation check if the app is in submitting state to disable the form button, and finally with useActionData display an error message if there is any

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 
 import { createOrder } from "../../services/apiRestaurant";
 
@@ -32,6 +32,9 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -59,6 +62,7 @@ function CreateOrder() {
           <div>
             <input type="tel" id="phone" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -81,7 +85,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>{isSubmitting ? "Placing order..." : "Order now"}</button>
         </div>
       </Form>
     </div>
@@ -97,12 +101,22 @@ export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
+  // Prepare the order object
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
 
+  // Error handling
+  const errors = {};
+
+  if (!isValidPhone(order.phone))
+    errors.phone = "Please give us your correct phone number. We might need it to contact you.";
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is correct, call the API to get the newly created order and redirect
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);

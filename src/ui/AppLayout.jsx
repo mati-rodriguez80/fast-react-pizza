a152c66b8c06a68397089caf14a1916bffb46939
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-06-14 19:05:00 -0300

    Start adding some margin, padding, border, and display, and add some styles for bigger screens from 640px and 768px always taking into account that tailwindcss expects from us to work mobile first

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,7 +9,7 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div>
       {isLoading && <Loader />}
 
       <Header />

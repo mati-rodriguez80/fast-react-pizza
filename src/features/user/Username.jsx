0282ed0fdc6cf1ce2fd8c79b0b65a3864742e98a
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-06-14 19:54:15 -0300

    Use flex and grid from tailwindcss to start building the layout of the app

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Matías</div>;
+  return <div className="hidden text-sm font-semibold md:block">Matías</div>;
 }
 
 export default Username;

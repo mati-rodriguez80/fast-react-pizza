ffa2ac727fec398ab0712493c44efc2ec3a8739b
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-06-27 20:08:32 -0300

    Create new async thunk in userSlice.js: the middleware will return the users position and address using reverse geocoding, then add extraReducers to the userSlice where we handle the three possible states for the thunk with three new reducers where we update four new pieces of state which are status, position, address, and error

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,32 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import { getAddress } from "../../services/apiGeocoding";
 
 function getPosition() {
+  // We are wrapping in a Promise the getCurrentPosition function to then be able to use async/await
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
 
-async function fetchAddress() {
-  // 1) We get the user's geolocation position
-  const positionObj = await getPosition();
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
+// By convention, these names for async thunks should not be called something with "get".
+// What is special about this is that this createAsyncThunk will basically produce three additional action types:
+// one for the Pending promise state, one for the Fulfill state, and one for the Rejected state. So now, we need to
+// handle these cases separately back in our reducers, and this is how we connect this thunk with our reducers.
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
 
-  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it in the
-  // order form, so that the user can correct it if wrong
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it in the
+    // order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-  // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-}
+    // 3) Then we return an object with the data that we are interested in
+    // Payload of the FULFILLED state
+    return { position, address };
+  },
+);
 
 const initialState = {
   username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -37,6 +50,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
